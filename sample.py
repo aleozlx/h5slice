c09284fdd272322e2fe80e997b0a5c728451c1fb
Alex Yang <zy5f9@mail.missouri.edu>
2018-08-26 13:40:38 -0500

    + postprocess

--- sample.py
+++ sample.py
@@ -13,6 +13,22 @@ def parse_query(query):
     except:
         pass
 
+def postprocess(im):
+    if len(im.shape) in [4, 3]:
+        mean = [103.939, 116.779, 123.68]
+        if im.shape[-1] == 1:
+            imout = (im[..., 0] + max(mean))
+        elif im.shape[-1] == 3:
+            imout = im[..., ::-1].copy()
+            imout[..., 0] += mean[0]
+            imout[..., 1] += mean[1]
+            imout[..., 2] += mean[2]
+        imout /= 255.0
+        np.clip(imout, 0.0, 1.0, out = imout)
+        return imout
+    else:
+        return im
+
 class H5Slice(protocol.Protocol):
     def dataReceived(self, data):
         fname, h5path, query, as_type = data.decode("ascii").split('\t')
@@ -20,7 +36,7 @@ class H5Slice(protocol.Protocol):
         idx = parse_query(query)
         as_type = as_type.strip()
         if idx and as_type in ('i4', 'f4'):
-            imtest = get_h5slice(fname, h5path, idx, as_type)
+            imtest = postprocess(get_h5slice(fname, h5path, idx, as_type))
             print('Out', imtest.shape, imtest.dtype, np.min(imtest), np.max(imtest))
             self.transport.write(gzip.compress(imtest.tobytes(), 3))
         self.transport.loseConnection()
